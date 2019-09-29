caec6c1ec554eb4669e2720acfae5d41e09f7fe3
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 02:48:48 -0400

    update show views

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -31,6 +31,10 @@ class PatientsController < ApplicationController
         end
     end
 
+    def show
+        find_patient
+    end
+
     private
     def find_patient
         @patient = Patient.find_by(id: params[:id])
