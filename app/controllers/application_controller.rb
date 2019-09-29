6d10755422dbb0fa7c608105e02c61aee8e65f34
latinawhocodes <alyssaroque01@gmail.com>
2019-09-28 20:00:10 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    private
+    def current_admin
+        @current_admin ||= Admin.find_by(id: session[:admin_id]) unless session[:admin_id].nil?
+    end
+
+    def logged_in?
+        !!current_admin
+    end
+
+    def require_login
+        redirect_to login_path if !logged_in?
+    end
 end
