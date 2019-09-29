88c2fe3bea5a1a84e9f52fe1077eb361c34ac4e8
latinawhocodes <alyssaroque01@gmail.com>
2019-09-28 22:23:59 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,12 @@ Rails.application.routes.draw do
 
 
   root 'sessions#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'admins#new'
+  post '/signup', to: 'admins#create'
+
 end
