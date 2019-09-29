5c90efa011692372437336d7dd7250f28573c374
latinawhocodes <alyssaroque01@gmail.com>
2019-09-28 20:07:38 -0400

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,28 @@
 class SessionsController < ApplicationController
 
+    def index
+    end
+
+    def new
+    end
+
+    def create
+        find_admin
+        if @admin.authenticate(params[:admin][:password])
+            session[:admin_id] = @admin.id
+            redirect_to admin_path(@admin.id)
+        else
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+    private
+    def find_admin
+        @admin = Admin.find_by(username: params[:admin][:username])
+    end
 end
