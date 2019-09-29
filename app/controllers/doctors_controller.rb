cd2b8234d25f8616bb7af1480a65aaefc0eb3d85
latinawhocodes <alyssaroque01@gmail.com>
2019-09-28 23:52:21 -0400

    Update doctors_controller.rb

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,36 @@
 class DoctorsController < ApplicationController
 
+    def index
+        @doctors = Doctor.all
+    end
+
+    def new
+        @doctor = Doctor.new
+    end
+
+    def create
+        find_doctor
+        @doctor = Doctor.new(doctor_params)
+        if @doctor.save
+            redirect_to doctor_path(@doctor)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        find_doctor
+    end
+
+    def update
+        find_doctor
+        if @doctor.update(doctor_params)
+            redirect_to doctor_path(@doctor)
+        else
+            render :edit
+        end
+    end
+
     private
     def doctor_params
         params.require(:doctor).permit(
