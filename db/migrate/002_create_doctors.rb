5735c1881dd839986191d6e8734650c8690b4e86
latinawhocodes <alyssaroque01@gmail.com>
2019-09-28 23:40:47 -0400

    Update 002_create_doctors.rb

--- db/migrate/002_create_doctors.rb
+++ db/migrate/002_create_doctors.rb
@@ -1,6 +1,15 @@
 class CreateDoctors < ActiveRecord::Migration
     def change
         create_table :doctors do |t|
+            t.string :first_name
+            t.string :last_name
+            t.string :specialty
+            t.string :gender
+            t.string :email_address
+
+            t.integer :admin_id
+
+            t.timestamps
         end
     end
 end
