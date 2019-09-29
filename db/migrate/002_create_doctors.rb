cfbdbb56e247360fc74cbdf0a10039a13f121316
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 01:57:16 -0400

    Update 002_create_doctors.rb

--- db/migrate/002_create_doctors.rb
+++ db/migrate/002_create_doctors.rb
@@ -1,4 +1,4 @@
-class CreateDoctors < ActiveRecord::Migration
+class CreateDoctors < ActiveRecord::Migration[5.2]
     def change
         create_table :doctors do |t|
             t.string :first_name
