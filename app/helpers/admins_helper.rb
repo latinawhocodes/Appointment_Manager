be7c3fb67685869a75b10fa8e7228fcbdfb76e05
latinawhocodes <alyssaroque01@gmail.com>
2019-09-28 22:44:30 -0400

    Create admins_helper.rb

---
+++ app/helpers/admins_helper.rb
@@ -0,0 +1,16 @@
+module AdminsHelper
+
+    def display_welcome
+        if logged_in?
+            content_tag(:h2, "Welcome, #{@admin.name.titlecase}!")
+        end
+    end
+
+    def display_admin_details
+        content_tag(:h4, "#{@admin.email.downcase}")
+        content_tag(:h4, "#{@admin.username.downcase}")
+        content_tag(:h5, "#{@admin.employee_id}")
+    end
+
+
+end
