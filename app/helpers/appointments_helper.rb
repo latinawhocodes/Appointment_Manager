419b31e295b2c6b2d2c4ad7617949e8c9bbcd986
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 02:15:07 -0400

    helpers updated

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,3 +1,6 @@
 module AppointmentsHelper
-
+    def doctor_form(form_builder)
+        #had a few problems with this form and referenced this from StackOverflow
+        render partial: "doctorform", locals: { d: form_builder }
+    end
 end
