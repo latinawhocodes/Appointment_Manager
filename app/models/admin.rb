cf12f6862828bed85d9028af5ff0c966cdff0834
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 00:48:33 -0400

    update admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,8 @@ class Admin < ApplicationRecord
     has_many :patients
     has_many :appointments, through: :patients
 
+    has_many :created_doctors, foreign_key: "admin_id", class_name: "Doctor"
+
     has_secure_password
 
     before_save do |admin|
