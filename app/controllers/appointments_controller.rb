14a68f54e1d42f9283a18205a887d60870ff7ddd
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 00:38:13 -0400

    Update appointments_controller.rb

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,9 +1,20 @@
 class AppointmentsController < ApplicationController
 
     def index
+        @appointments = @doctor.appointments if params[:doctor_id] && find_doctor
+
+        @appointments = @patient.appointments if params[:patient_id] && find_patient
+
+        @appointments = Appointment.all
     end
 
     def new
+        if params[:doctor_id] && find_doctor
+            @appointment = @doctor.appointments.build
+        else
+            @appointment = Appointment.new
+            @appointment.build_doctor
+        end
     end
 
     def create
@@ -17,12 +28,15 @@ class AppointmentsController < ApplicationController
 
     private
     def find_appointment
+        @appointment = Appointment.find(params[:id])
     end
 
     def find_doctor
+        @doctor = Doctor.find_by_id(params[:doctor_id])
     end
 
     def find_patient
+        @patient = Patient.find_by_id(params[:patient_id])
     end
 
     def appointment_params
