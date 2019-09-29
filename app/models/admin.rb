d2b6d1fb16f99324751a7f737b87a4933fff5c19
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 03:45:05 -0400

    Update admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -9,10 +9,18 @@ class Admin < ApplicationRecord
     validates :employee_id, uniqueness: true, presence: true
     validates :email, presence: true
 
-
     before_save do |admin|
         admin.username.downcase!
         admin.email.downcase!
         admin.name.titleize
+    end
+
+    def self.from_omniauth(auth)
+        #Google OmniAuth Documentation: https://medium.com/@rachel.hawa/google-authentication-strategy-for-rails-5-application-cd37947d2b1b
+        where(email: auth.info.email).first_or_initialize do |admin|
+          admin.username = auth.info.name
+          admin.email = auth.info.email
+          admin.password = SecureRandom.hex
+        end
     end
 end
