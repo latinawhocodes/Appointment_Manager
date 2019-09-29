fa3b54fa2ff237991969be47835fb801cb9ded25
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 02:22:59 -0400

    update appt views

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,6 +1,11 @@
 module AppointmentsHelper
+
     def doctor_form(form_builder)
         #had a few problems with this form and referenced this from StackOverflow
-        render partial: "doctorform", locals: { d: form_builder }
+        if @doctor
+            content_tag(:h2,"Appointments for Dr. #{@doctor.last_name}")
+        else
+            render partial: "doctorform", locals: { d: form_builder }
+        end
     end
 end
