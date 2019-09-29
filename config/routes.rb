1c196d64854c19e83492860d55eea9064087f7cb
latinawhocodes <alyssaroque01@gmail.com>
2019-09-28 22:14:37 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doctors do
+    resources :appointments, only: [:index, :new, :create]
+  end
+
+  resources :patients do
+    resources :appointments
+  end
+
+  resources :appointments
+  resources :admins
+
+
+  root 'sessions#index'
 end
