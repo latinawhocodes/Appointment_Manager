f2b68497f0b9a19647cee0bc6972ddf6173d0e29
latinawhocodes <alyssaroque01@gmail.com>
2019-09-29 00:06:57 -0400

    update patients_controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,35 @@
 class PatientsController < ApplicationController
 
+    def index
+        @patients = Patient.all
+        #ideally -> update this so patients are shown as: LASTNAME, FIRSTNAME, AGE
+    end
+
+    def new
+        @patient = Patient.new
+    end
+
+    def create
+        @patient = current_admin.patients.build(patient_params)
+        if @patient.save
+            redirect_to patient_path(@patient)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        find_patient
+    end
+
+    def update
+        find_patient
+        if @patient.update(patient_params)
+            redirect_to patient_path(@patient)
+        else
+            render :edit
+        end
+    end
 
     private
     def find_patient
